43799bb44fadf591341befb565a4abc3b3e4cc23
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2018-10-13 16:56:01 -0700

    Add docstring for Rule

--- new_rules/rules.py
+++ new_rules/rules.py
@@ -1,4 +1,9 @@
 class Rule:
+    """Rule is a piece of logic that can be executed to provide a Boolean result.
+    A rule accepts a payload which is `dict` that contains data attributes used
+    by the rule.  In addition, the execution returns an `str` object that
+    contains a finding, i.e., more details about the success or failure.
+    """
     def __init__(self, payload, name=None):
         self.payload = payload
         self.name = name

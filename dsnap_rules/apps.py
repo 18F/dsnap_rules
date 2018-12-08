a39f81de13cbc440e38034961a2de5b4a857cb2d
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2018-12-08 11:33:32 -0800

    Migrate from Flask to Django
    Use a more-batteries-included web framework for the API, given that we
    are also going to be building a web application to go with it.

---
+++ dsnap_rules/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DsnapRulesConfig(AppConfig):
+    name = 'dsnap_rules'
---
+++ dsnap_rules/views.py
@@ -0,0 +1,60 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from jsonschema.exceptions import ValidationError
+
+from .decorators import json_request
+from .validate import validate
+from .dsnap_rules import (
+    AdverseEffectRule,
+    AuthorizedRule,
+    ConflictingUSDAProgramRule,
+    FoodPurchaseRule,
+    IncomeAndResourceRule,
+    ResidencyRule,
+    SNAPSupplementalBenefitsRule,
+)
+from .rules import And
+from .models import Disaster
+
+
+@csrf_exempt
+@json_request
+def index(request):
+    data = request.json
+
+    try:
+        validate(data)
+    except ValidationError as ve:
+        response = jsonify(message=ve.message)
+        response.status_code = 400
+        return response
+
+    try:
+        disaster = Disaster.objects.get(
+            disaster_request_no=data["disaster_request_no"])
+    except Disaster.DoesNotExist:
+        response = jsonify(message="Disaster {} not found".format(
+            data["disaster_request_no"]))
+        response.status_code = 404
+        return response
+
+    result = And(
+        AuthorizedRule(),
+        AdverseEffectRule(),
+        FoodPurchaseRule(),
+        ResidencyRule(),
+        ConflictingUSDAProgramRule(),
+        SNAPSupplementalBenefitsRule(),
+        IncomeAndResourceRule()
+    ).execute(data, disaster)
+
+    return jsonify(
+        eligible=result.successful,
+        findings=result.findings,
+        metrics=result.metrics,
+        state_or_territory=disaster.state_or_territory
+    )
+
+
+def jsonify(**kwargs):
+    return JsonResponse(kwargs)

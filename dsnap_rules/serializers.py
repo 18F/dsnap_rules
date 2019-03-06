b1fc59e8177af615f0abfc2de625a92324ca1856
Vraj Mohan <radhakrishnan.vrajmohan@gsa.gov>
2019-03-05 16:37:25 -0800

    Conform to PEP8

--- dsnap_rules/serializers.py
+++ dsnap_rules/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import ApplicationPeriod, Disaster
 
+
 class CountyField(serializers.RelatedField):
     def to_representation(self, value):
         return f"{value.name}"
@@ -8,12 +9,15 @@ class CountyField(serializers.RelatedField):
 
 class ApplicationPeriodSerializer(serializers.ModelSerializer):
     counties = CountyField(many=True, read_only=True)
+
     class Meta:
         model = ApplicationPeriod
         fields = ('begin_date', 'end_date', 'counties')
 
+
 class DisasterSerializer(serializers.ModelSerializer):
     application_periods = ApplicationPeriodSerializer(many=True)
+
     class Meta:
         model = Disaster
         fields = '__all__'
